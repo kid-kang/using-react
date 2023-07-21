fe54d045964e3287bdeffbee264167dc5a7f2af6
sc9632 <1356422517@qq.com>
2023-07-21 17:47:16 +0800

    可编辑protable 90%

--- src/pages/AntdProTable/config.tsx
+++ src/pages/AntdProTable/config.tsx
@@ -10,11 +10,25 @@ export interface ProTableDataItem {
   phone: string;
 }
 
-// 造数据
-function setProTableData() {
-  const arr = [];
+interface FetchResType {
+  data: ProTableDataItem[];
+  success: boolean;
+  total: number;
+}
+
+interface ParamsType {
+  pageSize?: number;
+  current?: number;
+  sex?: '男' | '女';
+}
+
+// 模拟接口
+export async function fetchData(params: ParamsType) {
+  console.log('params: ', params);
+  // 造数据
+  let data: ProTableDataItem[] = [];
   for (let i = 0; i < 50; i++) {
-    arr.push({
+    data.push({
       key: i,
       name: `wzk${i}`,
       age: i,
@@ -23,23 +37,39 @@ function setProTableData() {
       phone: '13454546757',
     });
   }
-  return arr;
+
+  data = params.sex ? data.filter((val) => val.sex === params.sex) : data;
+
+  return await new Promise<FetchResType>((resolve) => {
+    setInterval(
+      () =>
+        resolve({
+          data,
+          success: true,
+          total: data.length,
+        }),
+      300
+    );
+  });
 }
 
-export function api(params = {}) {
-  console.log('params: ', params);
-  return Promise.resolve(setProTableData());
-}
-
+// columns配置
 export const columns: ProColumns<ProTableDataItem>[] = [
   {
-    title: '姓名',
-    dataIndex: 'name',
-    key: 'name',
-    width: 100,
-    copyable: true,
-    // hideInTable: true, // 写了这个就不是列  就是搜索组件
+    title: '序号',
+    valueType: 'indexBorder', //indexBorder就是proTable自动帮你生成的1，2，3，4的序号
+  },
+  {
     // fixed: 'left', // 固定列
+    title: '姓名', // 表头标题
+    dataIndex: 'name', // 对应返回的data数据中数组对象中的key
+    key: 'name', // 表头key
+    width: 140,
+    copyable: true, // 一键复制icon
+    search: false, // 是否显示该列的搜索表单
+    tip: '显示在表头的提示',
+    // hideInTable: true, // 写了这个就不是列  就是搜索组件
+    // 编辑时的校验
     formItemProps: {
       rules: [
         {
@@ -50,30 +80,41 @@ export const columns: ProColumns<ProTableDataItem>[] = [
     },
   },
   {
-    title: '年龄',
-    dataIndex: 'age',
-    key: 'age',
-    width: 80,
-    search: false,
-    sorter: (a, b) => a.age - b.age,
-  },
-  {
     title: '性别',
     dataIndex: 'sex',
     key: 'sex',
-    width: 80,
     valueType: 'select', // 定义搜索表单类型
+    filters: true, //表头的筛选菜单项，当值为 true 时，自动使用 valueEnum 生成
+    // onFilter不会触发request! 只是静态筛选符合的, 返回的值为false会过滤掉,每条数据都会判断
+    onFilter: true, // 同等于下面操作
+    // onFilter: (value, record) => {
+    //   // value是选中valueEnum中的key record是这条数据
+    //   return record.sex === value ? true : false;
+    // },
+    // 枚举select
     valueEnum: {
-      // 枚举select
-      nan: '男',
-      nv: '女',
+      男: {
+        text: '男', //text表示实际显示出来的文本
+        status: 'Success', //表示文字前面的那一个点是什么颜色
+      },
+      女: {
+        text: '女',
+        status: 'Error',
+      },
     },
+  },
+  {
+    title: '年龄',
+    dataIndex: 'age',
+    key: 'age',
+    search: false,
+    renderText: (val: number) => `${val} 岁`, // 展示数据额外文字
+    sorter: (a, b) => a.age - b.age, // 默认排序
   },
   {
     title: '住址',
     dataIndex: 'address',
     key: 'address',
-    width: 180,
     search: false,
     ellipsis: true, // 设置 ellipsis 可以让单元格内容根据宽度自动省略。
   },
@@ -81,16 +122,26 @@ export const columns: ProColumns<ProTableDataItem>[] = [
     title: '电话',
     dataIndex: 'phone',
     key: 'phone',
-    search: false, // 是否显示该列的搜索表单
+    search: false,
   },
   {
     title: '操作',
-    valueType: 'option', // 该类型不会产生搜索表单
     key: 'action',
+    valueType: 'option', // 该类型不会产生搜索表单
+    align: 'center', // 表头文字水平居中
     // fixed: 'right', // 固定列
-    width: 170,
-    render: () => {
-      return <Button>Edit</Button>;
+    render: (dom, record, _, action) => {
+      // render为自定义渲染, record为当前行数据
+      return (
+        <Button
+          onClick={() => {
+            console.log(dom);
+            action?.startEditable?.(record.key);
+          }}
+        >
+          Edit
+        </Button>
+      );
     },
   },
 ];
--- src/pages/AntdProTable/index.tsx
+++ src/pages/AntdProTable/index.tsx
@@ -1,21 +1,73 @@
-import { ProTable } from '@ant-design/pro-components';
-// import { useState } from 'react';
-import { columns, api } from './config';
+import { ProTable, ProForm, ProFormText } from '@ant-design/pro-components';
+import { PlusCircleOutlined } from '@ant-design/icons';
+import { Drawer, Button } from 'antd';
+import { useState } from 'react';
+import { columns, fetchData, type ProTableDataItem } from './config';
 
 export default function AntdProTable() {
+  const [open, setOpen] = useState(false);
+
+  function editRow(row: ProTableDataItem) {
+    console.log('row: ', row);
+    return fetchData({
+      pageSize: 10,
+      current: 1,
+      sex: '男',
+    });
+  }
+
   return (
     <div style={{ padding: '0 100rem' }}>
       <ProTable
-        cardBordered
-        columns={columns}
+        headerTitle='个人信息查询'
+        cardBordered // 板块板框
+        columns={columns} // 列配置
+        pagination={{ pageSize: 5 }} // 用于配置分页功能的相关参数，包括当前页码、每页显示的数据条数、总数据条数等
+        // params={params} // 额外设置request的请求参数
+        // search={false}  // false为不展示搜索板块, 也可以传入搜索按钮部分的配置
+        // options={false} // 用于配置表格的一些基本选项。这个属性可以设置为一个对象，包含多个选项，用于控制表格的分页、密度、搜索、导出等功能。
+        // onSubmit={(params) => {}} // 点击查询时触发,传入搜索的参数
+        request={fetchData} // 需要获取数据时，会调用"request"属性所指定的函数，并将当前的查询参数、排序规则、过滤条件等信息作为参数传递给这个函数。这个函数需要返回一个Promise对象，表示异步操作的结果。当ProTable组件获取到异步操作的结果后，会自动更新表格的数据源，并重新渲染表格。
+        // manualRequest = {true}  // 表示初次渲染不发请求
+
+        toolBarRender={() => [
+          <Button type='primary' shape='round' icon={<PlusCircleOutlined />} onClick={() => setOpen(true)}>
+            新增
+          </Button>,
+        ]} // 用于自定义表格工具栏的渲染内容。这个属性接受一个函数作为参数，用于渲染工具栏的内容。
+        rowSelection={{}} // 勾选 参考Table
+        // tableAlertRender={({ selectedRowKeys, selectedRows }) =>'自定义勾选文案'}
+        // tableAlertOptionRender={props => {
+        //   console.log(props);
+        //   return <div>自定义勾选操作</div>;
+        // }}
+
         editable={{
-          type: 'multiple',
-        }}
-        request={(params) => {
-          console.log(api(params));
-          return api(params);
+          // 配置row可编辑
+          type: 'single', // 能同时编辑多行还是单行 single | multiple
+          actionRender: (row, config, defaultDom) => {
+            console.log(1111, row, config, defaultDom);
+            return [defaultDom.save, defaultDom.cancel];
+          },
+          onSave: (_, row) => editRow(row),
         }}
       />
+
+      <Drawer
+        destroyOnClose // 用于控制弹窗是否在关闭时销毁组件实例。
+        title='新增行'
+        placement='right' // 出来的位子
+        onClose={() => setOpen(false)}
+        open={open}
+      >
+        <ProForm>
+          <ProFormText width='md' name='name' label='姓名' placeholder='请输入配姓名' rules={[{ required: true }]} />
+          <ProFormText width='md' name='age' label='年龄' rules={[{ required: true }]} />
+          <ProFormText width='md' name='sex' label='性别' rules={[{ required: true }]} />
+        </ProForm>
+      </Drawer>
     </div>
   );
 }
+
+// actionRef.current.reload()   //刷新表格，触发request属性拉取最新数据
